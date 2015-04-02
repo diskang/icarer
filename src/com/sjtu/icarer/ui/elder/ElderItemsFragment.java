6ace1c87d8f47895c034906c3aa59a4f48fc75b4
ksy <compu@sjtu.edu.cn>
2015-04-02 15:08:27 +0800

    last commit from cisco's laptop

--- src/com/sjtu/icarer/ui/elder/ElderItemsFragment.java
+++ src/com/sjtu/icarer/ui/elder/ElderItemsFragment.java
@@ -12,9 +12,11 @@ import com.sjtu.icarer.R;
 import com.sjtu.icarer.common.utils.LogUtils;
 import com.sjtu.icarer.common.utils.view.Toaster;
 import com.sjtu.icarer.core.LoadAreaItemTask;
+import com.sjtu.icarer.core.LoadElderItemTask;
 import com.sjtu.icarer.core.LoadElderTask;
 import com.sjtu.icarer.model.AreaItem;
 import com.sjtu.icarer.model.Elder;
+import com.sjtu.icarer.model.ElderItem;
 import com.sjtu.icarer.persistence.DbManager;
 import com.sjtu.icarer.ui.area.AreaItemAdapter;
 
@@ -55,6 +57,21 @@ public class ElderItemsFragment extends Fragment{
             protected void onSuccess(List<Elder> elders) throws Exception {
                 super.onSuccess(elders);
                 inflateEldersView(elders);
+            }
+			@Override
+    		protected void onException(final Exception e) throws RuntimeException {
+                super.onException(e);
+                LogUtils.d(e.getMessage());
+                e.printStackTrace();  
+            }
+		}.start();
+	}
+	
+	private void getElderItems(Elder elder){
+		new LoadElderItemTask(getActivity(),dbManager, elder, false){
+			@Override
+            protected void onSuccess(List<ElderItem> elderItems) throws Exception {
+                super.onSuccess(elderItems);
             }
 			@Override
     		protected void onException(final Exception e) throws RuntimeException {
